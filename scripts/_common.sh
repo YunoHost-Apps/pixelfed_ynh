5911a4fa767aead96146b665d2b5b8a13d130763
yalh76 <yalh@yahoo.com>
2019-03-31 17:51:19 +0200

    switch from imagick to gd

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 pkg_dependencies="php7.2-pgsql php7.2-mbstring php7.2-bcmath php7.2-simplexml php7.2-curl postgresql redis-server \
 php7.2-intl php7.2-exif \
 libfreetype6 libjpeg62-turbo libpng16-16 libxpm4 libvpx4 libwebp6 libmagickwand-6.q16-3 \
-php7.2-imagick imagick \
+php7.2-gd \
 pngquant jpegoptim gifsicle"
 
 #=================================================
