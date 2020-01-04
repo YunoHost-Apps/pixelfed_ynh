ecc58c5d5a24e6b4ae8e84475895d7625469ed5c
yalh76 <yalh@yahoo.com>
2020-01-04 14:14:50 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -104,7 +104,7 @@ ynh_install_php () {
 
 	# Pin this extra repository after packages are installed to prevent sury of doing shit
 	ynh_pin_repo --package="*" --pin="origin \"packages.sury.org\"" 200 --name=extra_php_version
-	ynh_pin_repo --package="php7.0*" --pin="origin \"packages.sury.org\"" 600 --name=extra_php_version $append
+	ynh_pin_repo --package="php7.0*" --pin="origin \"packages.sury.org\"" 600 --name=extra_php_version --append
 
 	# Advertise service in admin panel
 	yunohost service add php${phpversion}-fpm --log "/var/log/php${phpversion}-fpm.log"
