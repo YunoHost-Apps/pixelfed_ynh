c704b4ac20c9457e7621f764ed4e2d283cb16790
yalh76 <yalh@yahoo.com>
2020-04-21 02:17:36 +0200

    Splitting ynh_composer in is own file

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,53 +17,6 @@ extra_pkg_dependencies="php7.3-bcmath php7.3-ctype php7.3-curl php7.3-exif php7.
 # EXPERIMENTAL HELPERS
 #=================================================
 
-# Execute a command with Composer
-#
-# usage: ynh_composer_exec --phpversion=phpversion [--workdir=$final_path] --commands="commands"
-# | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
-# | arg: -c, --commands - Commands to execute.
-ynh_composer_exec () {
-	# Declare an array to define the options of this helper.
-	local legacy_args=vwc
-	declare -Ar args_array=( [v]=phpversion= [w]=workdir= [c]=commands= )
-	local phpversion
-	local workdir
-	local commands
-	# Manage arguments with getopts
-	ynh_handle_getopts_args "$@"
-	workdir="${workdir:-$final_path}"
-	phpversion="${phpversion:-7.0}"
-
-	COMPOSER_HOME="$workdir/.composer" \
-		php${phpversion} "$workdir/composer.phar" $commands \
-		-d "$workdir" --quiet --no-interaction
-}
-
-# Install and initialize Composer in the given directory
-#
-# usage: ynh_install_composer --phpversion=phpversion [--workdir=$final_path]
-# | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
-ynh_install_composer () {
-	# Declare an array to define the options of this helper.
-	local legacy_args=vw
-	declare -Ar args_array=( [v]=phpversion= [w]=workdir= )
-	local phpversion
-	local workdir
-	# Manage arguments with getopts
-	ynh_handle_getopts_args "$@"
-	workdir="${workdir:-$final_path}"
-	phpversion="${phpversion:-7.0}"
-
-	curl -sS https://getcomposer.org/installer \
-		| COMPOSER_HOME="$workdir/.composer" \
-		php${phpversion} -- --quiet --install-dir="$workdir" \
-		|| ynh_die --message="Unable to install Composer."
-
-	# update dependencies to create composer.lock
-	ynh_composer_exec --phpversion="${phpversion}" --workdir="$workdir" --commands="install --no-dev" \
-		|| ynh_die --message="Unable to update core dependencies with Composer."
-}
-
 # Install another version of php.
 #
 # usage: ynh_install_php --phpversion=phpversion [--package=packages]
