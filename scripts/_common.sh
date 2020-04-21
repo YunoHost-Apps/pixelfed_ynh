496f080193f9b85ad0e571b431f0d37c0ef03306
yalh76 <yalh@yahoo.com>
2020-04-21 05:44:31 +0200

    Replacing horizon systemd service by supervisor

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,10 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql redis-server imagemagick jpegoptim optipng pngquant"
+pkg_dependencies="postgresql redis-server imagemagick jpegoptim optipng pngquant supervisor"
 
-extra_pkg_dependencies="php7.3-bcmath php7.3-ctype php7.3-curl php7.3-exif php7.3-iconv php7.3-intl php7.3-json php7.3-mbstring php7.3-openssl php7.3-tokenizer php7.3-xml php7.3-zip php7.3-pgsql"
+extra_pkg_dependencies="php7.3-bcmath php7.3-ctype php7.3-curl php7.3-exif php7.3-iconv php7.3-intl php7.3-json php7.3-mbstring php7.3-tokenizer php7.3-xml php7.3-zip php7.3-pgsql"
+#  php7.3-openssl
 
 #=================================================
 # PERSONAL HELPERS
