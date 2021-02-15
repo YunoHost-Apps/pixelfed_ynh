c9d4bde63a994a25fa67ea81461b76d05565284e
yalh76 <yalh@yahoo.com>
2021-02-15 19:39:38 +0100

    update for v0.10.10

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql redis-server imagemagick jpegoptim optipng pngquant supervisor"
+pkg_dependencies="postgresql redis-server libgd3 jpegoptim optipng pngquant ffmpeg imagemagick supervisor"
 
 YNH_PHP_VERSION="7.3"
 
