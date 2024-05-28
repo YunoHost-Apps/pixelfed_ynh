d7ca2601b9229445576cd326f045540d634b32e6
NorbiPeti <norbipeti@norbipeti.eu>
2024-05-29 00:19:38 +0200

    Revert running Composer update as user
    It might break updates and doesn't seem to provide much benefit

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -184,34 +184,3 @@ ynh_supervisor_action() {
         ynh_clean_check_starting
     fi
 }
-
-# Execute a command with Composer
-#
-# usage: ynh_composer_exec [--phpversion=phpversion] [--workdir=$install_dir] --commands="commands"
-# | arg: -v, --phpversion - PHP version to use with composer
-# | arg: -w, --workdir - The directory from where the command will be executed. Default $install_dir or $final_path
-# | arg: -c, --commands - Commands to execute.
-#
-# Requires YunoHost version 4.2 or higher.
-ynh_composer_exec() {
-    local _globalphpversion=${phpversion-:}
-    # Declare an array to define the options of this helper.
-    local legacy_args=vwc
-    declare -Ar args_array=([v]=phpversion= [w]=workdir= [c]=commands=)
-    local phpversion
-    local workdir
-    local commands
-    # Manage arguments with getopts
-    ynh_handle_getopts_args "$@"
-    workdir="${workdir:-${install_dir:-$final_path}}"
-
-    if dpkg --compare-versions ${YNH_APP_PACKAGING_FORMAT:-0} lt 2; then
-        phpversion="${phpversion:-$YNH_PHP_VERSION}"
-    else
-        phpversion="${phpversion:-$_globalphpversion}"
-    fi
-
-    COMPOSER_HOME="$workdir/.composer" COMPOSER_MEMORY_LIMIT=-1 \
-        sudo -u $app php${phpversion} "$workdir/composer.phar" $commands \
-        -d "$workdir" --no-interaction --no-ansi 2>&1
-}
