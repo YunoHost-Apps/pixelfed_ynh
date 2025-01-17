6ad68d9137b4b381ce035471236fa8e920e8d95b
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-01-17 14:32:59 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,14 @@
 #=================================================
 
 composer_version="2.8.4"
+
+# Define a function to execute commands with `exec_artisan`
+exec_artisan() {
+  (cd "$install_dir" && ynh_exec_as_app php${php_version} artisan)
+}
+
+# Define a function to execute commands with `exec_artisan_as_root`
+exec_artisan_as_root() {
+  (cd "$install_dir" php${php_version} artisan)
+}
+
